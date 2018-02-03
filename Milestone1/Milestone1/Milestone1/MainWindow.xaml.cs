a7d9a0ae84110ef52ce8cb99444b6452e618bb86
lclar <l.clarke1991@gmail.com>
2018-02-03 12:48:28 -0800

    Working but with one bug

--- Milestone1/Milestone1/Milestone1/MainWindow.xaml.cs
+++ Milestone1/Milestone1/Milestone1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace Milestone1
 {
@@ -20,9 +21,173 @@ namespace Milestone1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class Business
+        {
+            public string name { get; set; }
+            public string state { get; set; }
+            public string city { get; set; }
+        }
+
+        private string buildConnString()
+        {
+            return "Host=localhost; Username=postgres; Password=Abigail1; Database=business;";
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            AddStates();
+            AddCities();
+            addColumns2Grid();
+        }
+
+        public void AddStates()
+        {
+            using (var comm = new NpgsqlConnection(buildConnString()))
+            {
+                comm.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = comm;
+                    cmd.CommandText = "SELECT DISTINCT state FROM business ORDER BY state;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            stateList.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                comm.Close();
+            }
+        }
+
+        public void AddCities()
+        {
+            using (var comm = new NpgsqlConnection(buildConnString()))
+            {
+                comm.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = comm;
+                    cmd.CommandText = "SELECT DISTINCT city FROM business ORDER BY city;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cityList.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                comm.Close();
+            }
+        }
+
+        public void AddCitiesWhenStateSelected()
+        {
+            using (var comm = new NpgsqlConnection(buildConnString()))
+            {
+                comm.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = comm;
+                    cmd.CommandText = "SELECT DISTINCT city FROM business WHERE state = '"+ stateList.SelectedItem.ToString()+"'ORDER BY city;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cityList.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                comm.Close();
+            }
+        }
+
+        /// <summary>
+        /// Add the columns to the grid
+        /// </summary>
+        public void addColumns2Grid()
+        {
+            DataGridTextColumn column = new DataGridTextColumn();
+            column.Header = "Business Name";
+            column.Width = 255;
+            column.Binding = new Binding("name");
+            businessGrid.Columns.Add(column);
+
+            DataGridTextColumn column2 = new DataGridTextColumn();
+            column2.Header = "State";
+            column2.Binding = new Binding("state");
+            businessGrid.Columns.Add(column2);
+
+            DataGridTextColumn column3 = new DataGridTextColumn();
+            column3.Header = "City";
+            column3.Binding = new Binding("city");
+            businessGrid.Columns.Add(column3);
+        }
+
+        /// <summary>
+        /// Add the businesses to the datagrid based on selected state
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            businessGrid.Items.Clear();
+
+            if (stateList.SelectedIndex > -1)
+            {
+                using (var comm = new NpgsqlConnection(buildConnString()))
+                {
+                    comm.Open();
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = comm;
+                        cmd.CommandText = "SELECT name,state,city FROM business WHERE state='" + stateList.SelectedItem.ToString() + "';";
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                businessGrid.Items.Add(new Business() { name = reader.GetString(0), state = reader.GetString(1), city = reader.GetString(2) });
+                            }
+                        }
+                    }
+                    comm.Close();
+                }
+                cityList.Items.Clear();
+                AddCitiesWhenStateSelected();
+            }
+        }
+
+        /// <summary>
+        /// Add the businesses to the datagrid based on selected city
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cityList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            businessGrid.Items.Clear();
+
+            if (cityList.SelectedIndex > -1)
+            {
+                using (var comm = new NpgsqlConnection(buildConnString()))
+                {
+                    comm.Open();
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = comm;
+                        cmd.CommandText = "SELECT name,state, city FROM business WHERE city='" + cityList.SelectedItem.ToString() + "' ORDER BY name;";
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                businessGrid.Items.Add(new Business() { name = reader.GetString(0), state = reader.GetString(1), city = reader.GetString(2) });
+                            }
+                        }
+                    }
+                    comm.Close();
+                }
+            }
         }
     }
 }
